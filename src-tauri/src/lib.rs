e71975e76ef446acb277930fdf8d0124519f1789
cPathz <9632611+cPathz@users.noreply.github.com>
2026-04-21 17:53:05 -0600

    Fix: add missing process plugin for update relaunch

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,6 +16,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             app.manage(ChildProcessMap(Arc::new(Mutex::new(HashMap::new()))));
             Ok(())
